0f56a3c9b6b019d12f31f20a8a9c5e36ea788f30
Brandon Antillon <brandon.antillon.681@my.csun.edu>
2016-06-27 18:19:43 -0600

    added chat bot

---
+++ discord_bot.py
@@ -0,0 +1,43 @@
+# discord chat bot
+# by: brandon lol
+
+import discord
+from discord.ext import commands
+import random
+from random import randint
+
+bot = discord.Client()
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print(bot.is_logged_in)
+    print('------')
+
+@bot.event
+async def on_message(message):
+    if message.content.startswith('!test'):
+        counter = 0
+        tmp = await bot.send_message(message.channel, 'Calculating messages...')
+        async for log in bot.logs_from(message.channel, limit=100):
+            if log.author == message.author:
+                counter += 1
+
+        await bot.edit_message(tmp, 'You have {} messages.'.format(counter))
+    elif message.content.startswith('!sleep'):
+        await asyncio.sleep(5)
+        await bot.send_message(message.channel, 'Done sleeping')
+    elif message.content.startswith('!roulette'):
+        tmpArr = [];
+        for server in bot.servers:
+            for member in server.members:
+                tmpArr.append(member)
+        arrInd = len(tmpArr)
+        randInd = randint(0, arrInd)
+        targetedUser = tmpArr[randInd]
+        await bot.send_message(message.channel, "BANG BANG!!!! {} is dead!".format(targetedUser))
+
+
+bot.run('')
