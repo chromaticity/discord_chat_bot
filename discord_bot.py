7a8f3afaaa622d807e85b749e3ec0d2f460cf80a
Brandon Antillon <brandon.antillon.681@my.csun.edu>
2016-06-27 22:29:25 -0600

    added funfact command

--- discord_bot.py
+++ discord_bot.py
@@ -1,5 +1,6 @@
 # discord chat bot
 # by: brandon lol
+# APIs: discord.py, spotipy8
 
 import discord
 from discord.ext import commands
@@ -29,9 +30,11 @@ async def on_message(message):
                 counter += 1
 
         await bot.edit_message(tmp, 'You have {} messages.'.format(counter))
+
     elif message.content.startswith('!sleep'):
         await asyncio.sleep(5)
         await bot.send_message(message.channel, 'Done sleeping')
+
     elif message.content.startswith('!roulette'):
         tmpArr = [];
         for server in bot.servers:
@@ -42,10 +45,12 @@ async def on_message(message):
         targetedUser = tmpArr[randInd]
         formattedUser = str(targetedUser).split("#")[0]
         await bot.send_message(message.channel, "BANG BANG!!!! {} is dead!".format(formattedUser))
+
     elif message.content.startswith('!toptracks'):
         # remember that you need to export the client id and all that other shit to the server...
         # as env variables
         # like so: 
+        await bot.send_message(message.channel, "Brandon's Top 5 Tracks - Time Period: Whenever")
         token = util.prompt_for_user_token("chroma_0", "user-top-read")
 
         if token:
@@ -55,10 +60,23 @@ async def on_message(message):
                 track = item['name']
                 artist = item['artists'][0]['name']
                 # print(track['name'] + ' - ' + track['artists'][0]['name'])
-                await bot.send_message(message.channel, "Brandon's Top 5 Tracks - Time Period: Whenever")
                 await bot.send_message(message.channel, track + " - " + artist)
         else:
             print("Can't get token for", username)
 
+    elif message.content.startswith('!funfact'):
+        # update this array
+        funFacts = ["Fun Fact: McIlhenny loves Perl.", "Fun Fact: CSUN is fucking garbage", "Fun Fact: Rafi taught me how 2 hack", 
+        "Fun Fact: My nuts itch", "Fun Fact: Rafa told me to make up shit for these facts", "Fun Fact: Dat Dao taught me literally everything for this major, I can drop out now if I fucking want to.", 
+        "Fun Fact: Dwayne Pascua's nickname is Dwayne Pascua", "Fun Fact: It's time to kick ass and display fun facts, and I'm all out of fun facts", 
+        "Fun Fact: HE DINDU NUFFIN", "Fun Fact: NippleShredder says 'Saw Dewd'", "Fun Fact: Nikolai thinks he shoulda done drugs and used his dick suckin mouth for big money and not perm numbers", 
+        "Fun Fact: You can use the !funfact command to throw out fun facts", "Fun Fact: The Mitochondria is the powerhouse of the cell"]
 
+        ffArrInd = len(funFacts)
+        randNum = randint(0, ffArrInd - 1)
+        funFact = funFacts[randNum]
+        # print fun fact i guess
+        await bot.send_message(message.channel, funFact)
+
+# get this api key from discord's site when you're logged in...
 bot.run('')
